558e8670739063132cb9fb7b6e32bfe1b42f1df5
blakerutledge <blake@blakerutledge.com>
2016-01-12 02:21:44 -0500

    fixed pesky bugs with svg builder, AMP&PM logic, current wind MPH overlap, svg margin, and killed 10day margin

--- js/main.js
+++ js/main.js
@@ -3,19 +3,13 @@
 var date, t, tt, time, ampm;
 var tickerVal = 0;
 var alertStatus = false;
+var hourForecast = [];
+var dayForecast = [];
+var alertMessage = "";
+var weatherInterval = 10 * 60000; //every 10 minutes
+var timeInterval = 1 * 60000; //every 1 minute
 
-var colors = [
-"#656869", "#616668", "#5D6466", "#596265", "#556063", "#505D62","#4E5C61", "#4C5B60", "#48595E", "#44575D",
-"#40555C", "#3B525A", "#375059", "#334E57", "#32505D", "#305262","#2F5568", "#2E576E", "#2C5973", "#2B5B79",
-"#2A5D7F", "#295F84", "#28628A", "#26648F", "#256694", "#24689A","#236AA0", "#216CA5", "#206FAB", "#1F71B1",
-"#1D73B6", "#1C75BC", "#1C7ABC", "#1C7FBC", "#1C84BC", "#1C89BC","#1C8EBC", "#1C93BC", "#2296B5", "#2999AE",
-"#2F9CA7", "#359EA0", "#3CA199", "#42A492", "#48A78B", "#4EAA84","#5BAF77", "#61B270", "#67B569", "#74BB5B",
-"#7ABE54", "#80C04D", "#87C346", "#8DC63F", "#91C840", "#96CA41","#9ACC43", "#9ECD44", "#A3CF45", "#A7D146",
-"#AAD046", "#ADCF46", "#B1CE46", "#B4CC45", "#B7CB45", "#BDC945","#C0C744", "#C6C544", "#C9C343", "#CFC143",
-"#D2C043", "#D6BF43", "#D9BD42", "#DFBB42", "#E4B942", "#E8B741","#EDB541", "#F2B441", "#F6B240", "#FBB040",
-"#F9AB3F", "#F7A63F", "#F5A23E", "#F39D3D", "#F1983D", "#EF933C","#ED8E3B", "#EB893B", "#E9843A", "#E78039",
-"#E57B39", "#E37638", "#E17137", "#DF6C37", "#DD6737", "#DA6236","#D85D36", "#D65835", "#D45334", "#D24E34",
-];
+var colors = [ "#656869", "#616668", "#5D6466", "#596265", "#556063", "#505D62","#4E5C61", "#4C5B60", "#48595E", "#44575D", "#40555C", "#3B525A", "#375059", "#334E57", "#32505D", "#305262","#2F5568", "#2E576E", "#2C5973", "#2B5B79", "#2A5D7F", "#295F84", "#28628A", "#26648F", "#256694", "#24689A","#236AA0", "#216CA5", "#206FAB", "#1F71B1", "#1D73B6", "#1C75BC", "#1C7ABC", "#1C7FBC", "#1C84BC", "#1C89BC","#1C8EBC", "#1C93BC", "#2296B5", "#2999AE", "#2F9CA7", "#359EA0", "#3CA199", "#42A492", "#48A78B", "#4EAA84","#5BAF77", "#61B270", "#67B569", "#74BB5B", "#7ABE54", "#80C04D", "#87C346", "#8DC63F", "#91C840", "#96CA41","#9ACC43", "#9ECD44", "#A3CF45", "#A7D146", "#AAD046", "#ADCF46", "#B1CE46", "#B4CC45", "#B7CB45", "#BDC945","#C0C744", "#C6C544", "#C9C343", "#CFC143", "#D2C043", "#D6BF43", "#D9BD42", "#DFBB42", "#E4B942", "#E8B741","#EDB541", "#F2B441", "#F6B240", "#FBB040", "#F9AB3F", "#F7A63F", "#F5A23E", "#F39D3D", "#F1983D", "#EF933C","#ED8E3B", "#EB893B", "#E9843A", "#E78039", "#E57B39", "#E37638", "#E17137", "#DF6C37", "#DD6737", "#DA6236","#D85D36", "#D65835", "#D45334", "#D24E34" ];
 
 var today = {
 	day: "",
@@ -41,8 +35,6 @@ function hourModule( h, i, t, tC, w, p, hu ) {
     this.humid = hu;
 };
 
-var hourForecast = [];
-
 function dayModule( d, i, h, hc, l, lc, p ) {
 	this.day = d,
 	this.icon = i,
@@ -52,14 +44,6 @@ function dayModule( d, i, h, hc, l, lc, p ) {
 	this.loColor = lc,
 	this.precip = p
 };
-
-var dayForecast = [];
-
-var alertStatus = false;
-var alertMessage = "";
-
-var weatherInterval = 10 * 60000; //every 10 minutes
-var timeInterval = 1 * 60000; //every 1 minute
 
 $( document ).ready(function() {
 	//DO NOW
@@ -147,11 +131,11 @@ function weatherCheck() {
 	  		var a = "none";
 	  		if ( parsed_json.alerts[0] != null){
 	  			//there's an alert!
-	  			a = parsed_json.alerts[0].description;
-	  			e = parsed_json.alerts[0].expires;
-	  			alertStatus = true;
-	  			var alert = 'Notice: '+ a +" until "+e+".";
-	  			writeAlert(alert);
+	  			// a = parsed_json.alerts[0].description;
+	  			// e = parsed_json.alerts[0].expires;
+	  			// alertStatus = true;
+	  			// var alert = 'Notice: '+ "a" +" until "+"e"+".";
+	  			// writeAlert(alert);
 	  		}
 	  		else {
 	  			//ALL IS WELL
@@ -172,12 +156,17 @@ function writeCurrent(data) {
 	$('.top_icon').addClass(data.icon);
 	$('.top_temp').html(data.temp);
 	$('.top_feels').html(data.feelsLike);
-	$('.top_wind').html(data.wind);
+	if ( data.wind < 10 ) {
+		$('.top_wind').html("0"+data.wind);	
+	}
+	else {
+		$('.top_wind').html(data.wind);		
+	}
 	$('.top_precip').html(data.precip);
 	$('.top_humid').html(data.humid);
 	$('#wrapperOut').css("background-color", data.tempColor)
 	$('.section_top').css("color", data.tempColor);
-	console.log(data.hour);
+	/*console.log(data.hour);
 	if (data.hour <= 6){
 		$('#wrapperOut').css("opacity", "0.3");
 	}
@@ -189,7 +178,7 @@ function writeCurrent(data) {
 	}
 	else if (21 <= data.hour){
 		$('#wrapperOut').css("opacity", "0.5");
-	}
+	}*/
 }
 
 function writeHourly(data) {
@@ -216,7 +205,7 @@ function writeHourly(data) {
 }
 
 function write10Day(data) {
-	for(var i=0; 10; i++) {
+	for(var i=0; i < 10; i++) {
 		$('.dailyModule'+i+' > h4').html(data[i].day);
 		$('.dailyModule'+i).find('.bot_icon').addClass(data[i].icon);
 		$('.dailyModule'+i).find('.dailyHi').html("<span>"+data[i].hi+"</span><span>&deg</span>");
@@ -283,15 +272,16 @@ function timeCheck() {
 	//time
 	date = new Date();
 	t = date.getHours();
-	if (t>12){
-		t=t-12;
+	if ( t>=12 ){
+		// t=t-12;
 		ampmBaby("pm");
 	}
-	if (t<=12){
+	if (t<12){
 		ampmBaby("am");
 	}
 	if (t==0){
 		t=12;
+		ampmBaby("am");
 	}
 	tt = date.getMinutes();
 	if (tt<10){
@@ -301,26 +291,22 @@ function timeCheck() {
 
 	//WRITE TIME
 	$('.top_time').html(time);
-	if (ampm=="AM"){
-		$(".pm").addClass("ampmDeactive");
-		$(".am").removeClass("ampmDeactive");
-	}
-	else {
-		$(".pm").removeClass("ampmDeactive");
-		$(".am").addClass("ampmDeactive");
-	}
 
 }
 
 function ampmBaby(x) {
 	//STYLE AM PM DIVS
-	if (x == "am") {
+	if (x === "am") {
 		//AM
 		ampm = "AM";
+		$(".pm").addClass("ampmDeactive");
+		$(".am").removeClass("ampmDeactive");
 	}
-	else if ( x == "pm" ) {
+	else {
 		//PM
 		ampm = "PM";
+		$(".pm").removeClass("ampmDeactive");
+		$(".am").addClass("ampmDeactive");
 	}
 }
 
