1a4419977834319d65a0f24afe8dbf496bffd336
blakerutledge <blake@blakerutledge.com>
2016-01-27 01:11:15 -0500

    add browser refresh every 24 hours

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var dayForecast = [];
 var alertMessage = "";
 var weatherInterval = 10 * 60000; //every 10 minutes
 var timeInterval = 1 * 60000; //every 1 minute
+var dayInterval = 24 * 60 * 60000; //every 24 hours
 
 var colors = [ "#656869", "#616668", "#5D6466", "#596265", "#556063", "#505D62","#4E5C61", "#4C5B60", "#48595E", "#44575D", "#40555C", "#3B525A", "#375059", "#334E57", "#32505D", "#305262","#2F5568", "#2E576E", "#2C5973", "#2B5B79", "#2A5D7F", "#295F84", "#28628A", "#26648F", "#256694", "#24689A","#236AA0", "#216CA5", "#206FAB", "#1F71B1", "#1D73B6", "#1C75BC", "#1C7ABC", "#1C7FBC", "#1C84BC", "#1C89BC","#1C8EBC", "#1C93BC", "#2296B5", "#2999AE", "#2F9CA7", "#359EA0", "#3CA199", "#42A492", "#48A78B", "#4EAA84","#5BAF77", "#61B270", "#67B569", "#74BB5B", "#7ABE54", "#80C04D", "#87C346", "#8DC63F", "#91C840", "#96CA41","#9ACC43", "#9ECD44", "#A3CF45", "#A7D146", "#AAD046", "#ADCF46", "#B1CE46", "#B4CC45", "#B7CB45", "#BDC945","#C0C744", "#C6C544", "#C9C343", "#CFC143", "#D2C043", "#D6BF43", "#D9BD42", "#DFBB42", "#E4B942", "#E8B741","#EDB541", "#F2B441", "#F6B240", "#FBB040", "#F9AB3F", "#F7A63F", "#F5A23E", "#F39D3D", "#F1983D", "#EF933C","#ED8E3B", "#EB893B", "#E9843A", "#E78039", "#E57B39", "#E37638", "#E17137", "#DF6C37", "#DD6737", "#DA6236","#D85D36", "#D65835", "#D45334", "#D24E34" ];
 
@@ -65,6 +66,10 @@ $( document ).ready(function() {
 		timeCheck();
 		ticker();
 	}, timeInterval);
+	setInterval(function () {
+		//every 24 hours, refresh browser (pi memory bug)
+		window.location.reload(true);
+	}, dayInterval);
 });
 
 function weatherCheck() {
