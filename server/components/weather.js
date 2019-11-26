2b9880e81ef8417ac149d7c25e16e49efc0c30cc
blakerutledge <blake@blakerutledge.com>
2019-11-25 23:40:57 -0500

    serverside data parsing, ingest into visualization and cleanup misc

--- server/components/weather.js
+++ server/components/weather.js
@@ -5,6 +5,8 @@ import fs from 'fs'
 
 let interval = undefined
 
+let days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+
 export let data = {}
 
 export let build = () => {
@@ -32,15 +34,17 @@ let fetch_weather = () => {
 
 		request( 'https://api.darksky.net/forecast/' + process.env.DARKSKY_SECRET_KEY + '/40.7133,-73.9509/', ( error, response, body ) => {
 
+			let filtered = filter( JSON.parse( body ) )
+
 			if ( error ) {
 				console.log( '🚫  error fetching and storing weather data' )
 				console.log( error )
 			}
 			else {
 				var _path = path.join( __dirname, '..', '..', 'data', 'weather.json' )
-				fs.writeFile( _path, body, 'utf8', () => {
+				jsonfile.writeFile( _path, filtered, 'utf8', () => {
 					console.log( '💾  wrote weather data to disk' )
-					data = JSON.parse( body )
+					data = filtered
 				} )
 
 			}
@@ -52,6 +56,104 @@ let fetch_weather = () => {
 
 }
 
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - FILTERS
+
+let filter = ( body ) => {
+
+	let obj = {
+		current: filter_current( body ),
+		hourly: filter_hourly( body ),
+		forecast: filter_forecast( body ),
+		raw: body
+	}
+
+	return obj
+
+}
+
+let filter_current = ( response ) => {
+
+	let output = {}
+
+	var c = 				response.currently
+
+	output.icon = 			"wi-" + c.icon
+	output.temp = 			Math.round( c.temperature )
+	output.feels_like = 	Math.round( c.apparentTemperature )
+	output.wind = 			Math.round( c.windSpeed )
+	output.precip = 		Math.round( 100 * c.precipProbability )
+	output.humid = 			Math.round( 100 * c.humidity )
+
+	return output
+
+}
+
+
+let filter_hourly = ( response ) => {
+
+	let output = []
+	
+	let offset = 0
+
+	let d = new Date( response.currently.time )
+	for ( let i=0; i<24; i++ ) {
+		let x = response.hourly.data[ i ]
+		let dd = new Date( x.time * 1000 )
+		if ( dd.getHours() == d.getHours() ) {
+			offset = i
+		}
+	}
+
+	for ( let i=0; i<24; i++ ) {
+		
+		let x = response.hourly.data[ i + offset ]
+		let dd = new Date( x.time * 1000 )
+
+		output.push( {
+			hour: 		dd.getHours(),
+			icon: 		"wi-" + x.icon,
+			temp: 		Math.round( x.temperature ),
+			wind: 		Math.round( x.windSpeed ),
+			precip: 	Math.round( 100 * x.precipProbability ),
+			humid: 		Math.round( 100 * x.humidity )
+		} )
+	}
+
+	return output
+
+}
+
+
+let filter_forecast = (response ) => {
+
+	let output = []
+
+	response.daily.data.forEach( ( x, i ) => {
+
+		var d = new Date( x.time * 1000 )
+
+		output.push( {
+			day: 		days[ d.getDay() ][ 0 ],
+			icon: 		"wi-" + x.icon,
+			hi: 		Math.round( x.temperatureHigh ),
+			lo: 		Math.round( x.temperatureLow ),
+			precip: 	Math.round( 100 * x.precipProbability ),
+		} )
+
+	} )
+
+	return output
+
+}
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ENDPOINT
 
 export let api = {
 
